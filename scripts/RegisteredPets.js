4cded46a613515c898449eecd80a613a5eb08632
Tom Clouse <tommyclouse@yahoo.com>
2021-04-26 10:45:41 -0500

    who walks whom

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,36 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pet")) {
+            const [, petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (parseInt(petId) === pet.id) {
+                    for (const walker of walkers) {
+                        if (pet.walkerId === walker.id) {
+                            window.alert(`${pet.name} is being walked by ${walker.name}.`)
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+)
+
+//On Monday, I should try to do this with the .find () array method to avoid the nested loop.
 
 const pets = getPets()
+const walkers = getWalkers()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
